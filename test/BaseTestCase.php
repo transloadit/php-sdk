c43b2c8dc20809cafebe1ccd3e583e602fb35a24
Felix Geisendörfer <felix@debuggable.com>
2010-12-17 20:14:00 +0100

    Move lib/*.php -> lib/transloadit/*.php

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -1,6 +1,6 @@
 <?php
 define('TEST_ROOT_DIR', dirname(dirname(__FILE__)));
-define('TEST_LIB_DIR', TEST_ROOT_DIR.'/lib');
+define('TEST_LIB_DIR', TEST_ROOT_DIR.'/lib/Transloadit');
 define('TEST_TEST_DIR', TEST_ROOT_DIR.'/test');
 define('TEST_FIXTURE_DIR', TEST_TEST_DIR.'/fixture');
 
