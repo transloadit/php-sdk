3623a9fd9bf3974641d8c0e5de0126c5938cfdfd
Raymond Julin <raymond.julin@gmail.com>
2013-04-24 10:39:27 +0200

    Make examples work again

--- example/3-assembly-form-with-jquery-plugin.php
+++ example/3-assembly-form-with-jquery-plugin.php
@@ -1,6 +1,9 @@
 <?php
-require_once('./config/common.php');
-$transloadit = new Transloadit($exampleConfig);
+$config = require __DIR__ . '/config/common.php';
+
+use transloadit\Transloadit;
+
+$transloadit = new Transloadit($config);
 /*
 ### 3. Integrate the jQuery plugin into the previous example
 
