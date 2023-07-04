5e74c938853a11689a72a926f61a0f6f65978731
Kevin van Zonneveld <kevin@vanzonneveld.net>
2023-07-04 16:21:06 +0200

    Pass down curlOptions when TransloaditRequest reinstantiates for _waitForCompletion

---
+++ examples/7-disable-ssl-verification.php
@@ -0,0 +1,37 @@
+<?php
+
+// Never do this!
+
+// To run: source env.sh && php examples/7-disable-ssl-verification.php
+
+require __DIR__ . '/common/loader.php';
+
+use transloadit\Transloadit;
+
+$transloadit = new Transloadit([
+  'key'    => getenv('YOUR_TRANSLOADIT_KEY'),
+  'secret' => getenv('YOUR_TRANSLOADIT_SECRET'),
+]);
+
+$response = $transloadit->createAssembly([
+  'files' => [dirname(__FILE__) . '/fixture/straw-apple.jpg'],
+  'curlOptions' => [
+    CURLOPT_SSL_VERIFYHOST => 0,
+    CURLOPT_SSL_VERIFYPEER => 0,
+  ],
+  'waitForCompletion' => true,
+  'params' => [
+    'steps' => [
+      'resize' => [
+        'robot' => '/image/resize',
+        'width' => 200,
+        'height' => 100,
+      ],
+    ],
+  ],
+]);
+
+// Show the results of the assembly we spawned
+echo '<xmp>';
+print_r($response);
+echo '</xmp>';
