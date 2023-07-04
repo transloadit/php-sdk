7bd5ba0728763ed45ea612be39222cf0d8230d40
Kevin van Zonneveld <kevin@vanzonneveld.net>
2023-07-04 16:24:50 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ if ! grep "${VERSION}" CHANGELOG.md > /dev/null 2>&1; then
   echo "First add '${VERSION}' to CHANGELOG.md please"
   exit 1
 fi
-if [ -n "$(git status --porcelain)" ]; then 
+if [ -n "$(git status --porcelain)" ]; then
   echo "Git working tree not clean. First commit all your work please."
   exit 1
 fi
