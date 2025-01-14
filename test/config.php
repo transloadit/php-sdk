4eabc08bfb2cd8744c35d13238587190379a0e4e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2025-01-14 21:06:02 +0100

    Update config.php

--- test/config.php
+++ test/config.php
@@ -1,14 +1,17 @@
 <?php
 
-$env = @file_get_contents('.env');
+$env = @file_get_contents(dirname(__FILE__) . '/../.env');
 if ($env) {
   foreach (explode("\n", $env) as $line) {
     if (trim($line) == '' || str_starts_with($line, '#')) {
       continue;
     }
     list($key, $value) = explode('=', $line, 2);
+    $key = str_replace('export ', '', $key);
+    $value = str_replace('"', '', str_replace("'", '', $value));
 
-    define(str_replace('export ', '', $key), str_replace('"', '', str_replace("'", '', $value)));
+    define($key, $value);
+    putenv("$key=$value");
   }
 } else {
   if (getenv('TRANSLOADIT_KEY') && getenv('TRANSLOADIT_SECRET')) {
